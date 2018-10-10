f98255c7ccd17c1bdba6c1048fca531f8d68e177
Karl Zhang <zhangye9416@163.com>
2018-10-10 22:14:55 +0800

    Fix CUDA_VISIBLE_DEVICES

--- scripts/bash/TransE.sh
+++ scripts/bash/TransE.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ~/wangrj/tensorflow/bin/activate
-CUDA_VISIBILE_DEVICE=0 python ../kg_train.py --method=TransE --config=../config/TransE.config
+CUDA_VISIBILE_DEVICES="0" python ../kg_train.py --method=TransE --config=../config/TransE.config
