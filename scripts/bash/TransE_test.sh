81510914e93e96b47ca22db5db012a00574d68f5
Karl Zhang <zhangye9416@163.com>
2018-11-06 19:27:54 +0800

    Modify config generator for new test

--- scripts/bash/TransE_test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source ~/wangrj/tensorflow/bin/activate
-CUDA_VISIBLE_DEVICES="1" python ../kg_train.py --method=TransE --config=../config/TransE_test.config --order=1
--- scripts/generator/transe_generator.py
+++ scripts/generator/transe_generator.py
@@ -8,20 +8,20 @@ def buildString(params):
     return s[:-1] + '\n'
 
 
-def generate(dataset):
-    configName = 'TransE'
+def generate(methodKey, config):
+    configName = 'TransE_' + methodKey
     f = open('../config/%s.config' % configName, 'w')
 
-    globalParams = {'threads': 32, 'dataset': 'ACE17K' if dataset is None else dataset}
+    globalParams = {'threads': threads, 'dataset': dataset}
     f.write(buildString(globalParams))
 
     count = 0
-    for epoch in [5000, 2500, 1500]:
-        for nbatches in [100]:
-            for alpha in [0.001]:
-                for margin in [2]:
-                    for bern in [0]:
-                        for dimension in [1000, 500, 250]:
+    for nbatches in config['nbatches']:
+        for bern in config['bern']:
+            for alpha in config['alpha']:
+                for margin in config['margin']:
+                    for epoch in config['epoch']:
+                        for dimension in config['dimension']:
                             f.write(buildString({
                                 'epoch': epoch,
                                 'nbatches': nbatches,
@@ -39,96 +39,45 @@ def generate(dataset):
     f.write('source ~/wangrj/tensorflow/bin/activate\n')
     for i in range(count):
         f.write(
-            'CUDA_VISIBLE_DEVICES="1" python ../kg_train.py --method=TransE --config=../config/%s.config --order=%i\n' % (
-                configName, i + 1))
-    f.close()
-
-
-def generateDetailed(dataset):
-    configName = 'TransE_detailed'
-    f = open('../config/%s.config' % configName, 'w')
-
-    globalParams = {'threads': 32, 'dataset': 'ACE17K' if dataset is None else dataset}
-    f.write(buildString(globalParams))
-
-    count = 0
-    for epoch in [5000]:
-        for nbatches in [100]:
-            for alpha in [0.0005, 0.001, 0.002, 0.01]:
-                for margin in [1, 2, 3, 4]:
-                    for bern in [0]:
-                        for dimension in [250]:
-                            f.write(buildString({
-                                'epoch': epoch,
-                                'nbatches': nbatches,
-                                'alpha': alpha,
-                                'margin': margin,
-                                'bern': bern,
-                                'dimension': dimension
-                            }))
-                            count += 1
-
-    f.close()
-
-    f = open('../bash/%s.sh' % configName, 'w')
-    f.write('#!/usr/bin/env bash\n')
-    f.write('source ~/wangrj/tensorflow/bin/activate\n')
-    for i in range(count):
-        f.write(
-            'CUDA_VISIBLE_DEVICES="1" python ../kg_train.py --method=TransE --config=../config/%s.config --order=%i\n' % (
-                configName, i + 1))
-    f.close()
-
-
-def generateWeighted(dataset):
-    configName = 'WTransE'
-    f = open('../config/%s.config' % configName, 'w')
-
-    globalParams = {'threads': 32, 'dataset': 'ACE17K' if dataset is None else dataset}
-    f.write(buildString(globalParams))
-
-    count = 0
-    for epoch in [10]:
-        for nbatches in [100]:
-            for alpha in [0.001]:
-                for margin in [2]:
-                    for bern in [0]:
-                        for dimension in [50]:
-                            f.write(buildString({
-                                'epoch': epoch,
-                                'nbatches': nbatches,
-                                'alpha': alpha,
-                                'margin': margin,
-                                'bern': bern,
-                                'dimension': dimension
-                            }))
-                            count += 1
-
-    f.close()
-
-    f = open('../bash/%s.sh' % configName, 'w')
-    f.write('#!/usr/bin/env bash\n')
-    f.write('source ~/wangrj/tensorflow/bin/activate\n')
-    for i in range(count):
-        f.write(
-            'CUDA_VISIBLE_DEVICES="1" python ../kg_train.py --method=TransE --weighted=True --config=../config/%s.config --order=%i\n' % (
-                configName, i + 1))
+            'CUDA_VISIBLE_DEVICES="%s" python ../kg_train.py --method=TransE --config=../config/%s.config --order=%i\n' % (
+                cuda, configName, i + 1))
     f.close()
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, required=False)
-parser.add_argument('--detailed', type=bool, required=False)
-parser.add_argument('--weighted', type=bool, required=False)
+parser.add_argument('--method', type=str, required=False)
+parser.add_argument('--threads', type=int, required=False)
+parser.add_argument('--cuda', type=str, required=False)
 parsedConfig = parser.parse_args()
 
-detailed = parsedConfig.detailed if parsedConfig.detailed else False
-weighted = parsedConfig.weighted if parsedConfig.weighted else False
+dataset = parsedConfig.dataset if parsedConfig.dataset else 'ACE17K'
+method = (parsedConfig.method if parsedConfig.method else 'all').lower()
+threads = parsedConfig.threads if parsedConfig.threads else 32
+cuda = parsedConfig.cuda if parsedConfig.cuda else '1'
 
-if not detailed:
-    if not weighted:
-        generate(parsedConfig.dataset)  # Test epoch & dimension
-    else:
-        generateWeighted(parsedConfig.dataset)
+paramConfig = {'basic': {'epoch': [2000, 1000],
+                         'dimension': [300, 200, 100],
+                         'nbatches': [100, 200],
+                         'margin': [3.0, 2.0, 1.0],
+                         'alpha': [0.001, 0.003, 0.01],
+                         'bern': [0, 1]},
+               'advanced': {'epoch': [5000, 3500, 2000],
+                            'dimension': [800, 500, 300],
+                            'nbatches': [100],
+                            'margin': [2.0],
+                            'alpha': [0.001],
+                            'bern': [0]},
+               'detailed': {'epoch': [5000],
+                            'dimension': [500],
+                            'nbatches': [100],
+                            'margin': [1.5, 2.0, 2.5, 3.0],
+                            'alpha': [0.0005, 0.001, 0.003, 0.01],
+                            'bern': [0]}
+               }
+
+if method == 'all':
+    for methodKey in paramConfig.keys():
+        generate(methodKey, paramConfig[methodKey])
 else:
-    generateDetailed(parsedConfig.dataset)  # Test alpha & margin
+    generate(method, paramConfig[method])
