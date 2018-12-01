16b712fd35de5065aeb584250d8b9d5178e8cca6
Karl Zhang <zhangye9416@163.com>
2018-12-01 10:36:24 +0800

    Change bash script for combined prediction

--- scripts/processor/combined_prediction_mindist.sh
+++ scripts/processor/combined_prediction_mindist.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-python relation_combination.py --method=ComplEx_advanced --order=2 --predict=True --update=True --alg=mindist --coeff=0.05
-python relation_combination.py --method=ComplEx_advanced --order=2 --predict=True --update=True --alg=mindist --coeff=0.1
-
-python relation_combination.py --method=WComplEx_advanced --order=6 --predict=True --update=True --alg=mindist
-
 python relation_combination.py --method=TransE_detailed --order=6 --predict=True --update=True --alg=mindist --coeff=0.05
 python relation_combination.py --method=TransE_detailed --order=6 --predict=True --update=True --alg=mindist --coeff=0.1
 
@@ -13,11 +8,11 @@ python relation_combination.py --method=TransH_test --order=1 --predict=True --u
 python relation_combination.py --method=DistMult_detailed --order=1 --predict=True --update=True --alg=mindist --coeff=0.05
 python relation_combination.py --method=DistMult_detailed --order=1 --predict=True --update=True --alg=mindist --coeff=0.1
 
+python relation_combination.py --method=TransH_test --order=1 --predict=True --update=True --alg=maxmrr
+python relation_combination.py --method=ComplEx_advanced --order=2 --predict=True --update=True --alg=maxmrr
+
 python relation_combination.py --method=WTransH_test --order=1 --predict=True --update=True --alg=mindist --coeff=0.05
 python relation_combination.py --method=WTransH_test --order=1 --predict=True --update=True --alg=mindist --coeff=0.1
 
 python relation_combination.py --method=WTransE_test --order=1 --predict=True --update=True --alg=mindist --coeff=0.05
 python relation_combination.py --method=WTransE_test --order=1 --predict=True --update=True --alg=mindist --coeff=0.1
-
-python relation_combination.py --method=WTransH_test --order=1 --predict=True --update=True --alg=mindist
-python relation_combination.py --method=WTransE_test --order=1 --predict=True --update=True --alg=mindist
