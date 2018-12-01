87e73075c249c9dbe1192a22edc87274ac1c4f4d
Karl Zhang <zhangye9416@163.com>
2018-12-01 10:42:16 +0800

    Update config and scripts for new_relation training

--- scripts/new_relation_bash/ComplEx_advanced.sh
+++ scripts/new_relation_bash/ComplEx_advanced.sh
@@ -9,3 +9,11 @@ CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --conf
 CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=6
 CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=7
 CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=8
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=9
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=10
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=11
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=12
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=13
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=14
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=15
+CUDA_VISIBLE_DEVICES="1" python kg_train_new_relation.py --method=ComplEx --config=./new_relation_config/ComplEx_advanced.config --order=16
